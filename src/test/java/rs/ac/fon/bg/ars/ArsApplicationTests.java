9dab66b0477fc0797d765f718bdc147593d4c36d
Petar2518 <pm20180025@student.fon.bg.ac.rs>
2024-03-17 00:51:29 +0100

    Initialization of project

---
+++ src/test/java/rs/ac/fon/bg/ars/ArsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.fon.bg.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
